2f9d8501bba7ca533db69f934f629ea02ef13f11
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2024-11-01 13:33:49 +0100

    Initial commit

---
+++ src/pages/NewEvent.js
@@ -0,0 +1,9 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+export default function NewEventPage() {
+  return (
+    <>
+      <EventForm method={"post"} />
+    </>
+  );
+}
